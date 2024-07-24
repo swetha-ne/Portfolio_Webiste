e16c946ba43882b378cd11b7b24ea23c1de5c23c
Swetha <snethmi18@gmail.com>
2024-07-24 12:33:32 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import '../styles/Header.css';
 import { FaLinkedin, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 // Logo image import
-import logo from '../assets/logo.png'; // Ensure you have this image in the assets folder
+import logo from '../assets/logopp.png'; // Ensure you have this image in the assets folder
 
 const Header = () => (
     <header>
@@ -13,10 +13,10 @@ const Header = () => (
             <img src={logo} alt="Logo" className="logo" />
             <nav>
                 <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#home">HOME</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#projects">PROJECTS</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
                 </ul>
             </nav>
             <div className="social-icons">
